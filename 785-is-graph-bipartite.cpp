6c7e6c4fd9e13ddd9155b63b61ac20e3de88d7b7
Akash Biswas <akash41bt@gmail.com>
2025-07-23 21:37:48 +0600

    solve LeetCode 785: graph coloring technique + BFS

--- 785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite.cpp
@@ -1,25 +1,38 @@
-class Solution {
+class Solution
+{
 public:
-    bool checkBipartite(vector<vector<int>>& graph, vector<int> &color, int u, int currColor){
+    bool checkBipartite(vector<vector<int>> &graph, vector<int> &color, int u, int currColor)
+    {
         color[u] = currColor;
+        queue<int> q;
+        q.push(u);
 
-        for(auto neigh: graph[u]){
-            if(color[neigh] == currColor)
-                return true;
-            if(color[neigh] == -1){
-                color[neigh] = 1 ^ currColor;
-                if(checkBipartite(graph, color, neigh, color[neigh]))
+        while (!q.empty())
+        {
+            u = q.front();
+
+            for (auto neigh : graph[u])
+            {
+                if (color[neigh] == color[u])
                     return true;
+                if (color[neigh] == -1)
+                {
+                    color[neigh] = 1 ^ color[u];
+                    q.push(neigh);
+                }
             }
+            q.pop();
         }
         return false;
     }
 
-    bool isBipartite(vector<vector<int>>& graph) {
+    bool isBipartite(vector<vector<int>> &graph)
+    {
         vector<int> color(graph.size(), -1);
 
-        for(int i = 0; i < graph.size(); i++){
-            if(color[i] == -1 && checkBipartite(graph, color, i, 1))
+        for (int i = 0; i < graph.size(); i++)
+        {
+            if (color[i] == -1 && checkBipartite(graph, color, i, 1))
                 return false;
         }
         return true;

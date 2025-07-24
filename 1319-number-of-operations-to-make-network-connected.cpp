70cfc4a2c9e2d547c7123c19e43f2b0fad770889
Akash Biswas <akash41bt@gmail.com>
2025-07-25 01:41:06 +0600

    solve LeetCode 1319: using disjoint set union (dsu)

--- 1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected.cpp
@@ -1,55 +1,43 @@
-class Solution
-{
+class Solution {
 public:
-    int extraEdge = 0, dis_component = 0;
+    int find(int i, vector<int> &parent){
+    if(parent[i] == -1)
+        return i;
+    return parent[i]=find(parent[i], parent);
+    }
 
-    void bfs(unordered_map<int, vector<int>> &adj, vector<bool> &vis, int u)
-    {
-        vis[u] = true;
-        queue<pair<int, int>> q;
-        q.push({u, -1});
-
-        while (!q.empty())
-        {
-            u = q.front().first;
-            int parent = q.front().second;
-
-            for (auto neigh : adj[u])
-            {
-                if (vis[neigh] && neigh != parent)
-                {
-                    extraEdge++;
-                }
-                if (!vis[neigh])
-                {
-                    vis[neigh] = true;
-                    q.push({neigh, u});
-                }
+    void _union(int a, int b, vector<int> &parent, vector<int> &rank){
+        int root_a = find(a, parent);
+        int root_b = find(b, parent);
+        if(root_a != root_b){
+            if(rank[root_a] > rank[root_b])
+                parent[root_b] = root_a;
+            else if(rank[root_a] < rank[root_b])
+                parent[root_a] = root_b;
+            else{
+                parent[root_a] = root_b;
+                rank[root_b]++;
             }
-            q.pop();
         }
     }
 
-    int makeConnected(int n, vector<vector<int>> &connections)
-    {
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
         unordered_map<int, vector<int>> adj;
+        vector<int> parent (n, -1);
+        vector<int> rank (n, 0);
 
-        for (auto edge : connections)
-        {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-        }
-        vector<bool> vis(n, false);
+        int extraEdge = 0;
 
-        for (int i = 0; i < n; i++)
-        {
-            if (!vis[i])
-            {
-                bfs(adj, vis, i);
-                dis_component++;
-            }
+        for(auto edge: connections){
+            if(find(edge[0], parent) == find(edge[1], parent))
+                extraEdge++;
+            else
+                _union(edge[0], edge[1], parent, rank);
         }
-        extraEdge /= 2;
-        return (extraEdge >= dis_component - 1) ? dis_component - 1 : -1;
+        
+        int dis_component = count(parent.begin(), parent.end(), -1);
+    
+        return (extraEdge >= dis_component - 1) ? dis_component - 1: -1;
     }
 };

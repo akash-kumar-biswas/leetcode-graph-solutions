e7aa50ffd2a02546f2069ee6dd88ee387718ac38
Akash Biswas <akash41bt@gmail.com>
2025-07-25 05:55:00 +0600

    solve LeetCode 2316: using DFS

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,53 +1,53 @@
-class Solution {
+class Solution
+{
 public:
-    int find(int i, vector<int> &parent){
-    if(parent[i] == i)
-        return i;
-    return parent[i]=find(parent[i], parent);
-    }
-
-    void _union(int a, int b, vector<int> &parent, vector<int> &rank){
-        int root_a = find(a, parent);
-        int root_b = find(b, parent);
-        if(root_a != root_b){
-            if(rank[root_a] > rank[root_b])
-                parent[root_b] = root_a;
-            else if(rank[root_a] < rank[root_b])
-                parent[root_a] = root_b;
-            else{
-                parent[root_a] = root_b;
-                rank[root_b]++;
+    void dfs(unordered_map<int, vector<int>> &adj, vector<bool> &vis, int u, int &cnt)
+    {
+        vis[u] = true;
+        cnt++;
+        for (auto neigh : adj[u])
+        {
+            if (!vis[neigh])
+            {
+                dfs(adj, vis, neigh, cnt);
             }
         }
     }
 
-    long long countPairs(int n, vector<vector<int>>& edges) {
-        vector<int> parent(n);
-        vector<int> rank(n, 0);
+    long long countPairs(int n, vector<vector<int>> &edges)
+    {
+        unordered_map<int, vector<int>> adj;
 
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
+        for (auto edge : edges)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
 
-        for(auto edge: edges){
-            _union(edge[0], edge[1], parent, rank);
-        }
+        vector<long long> comp_size;
+        vector<bool> vis(n, false);
 
-        unordered_map<int, long long> ump;
-
-        for(int i = 0; i < n; i++){
-            ump[find(i, parent)]++;
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                int cnt = 0;
+                dfs(adj, vis, i, cnt);
+                comp_size.push_back(cnt);
+            }
         }
 
         long long ans = 0;
-        
-        if(ump.size() == 1)
+
+        if (comp_size.size() == 1)
             return 0;
+
         int included_nodes = 0;
 
-        for(auto it: ump){
-            ans += it.second*(n - included_nodes  - it.second);
-            included_nodes += it.second;
+        for (auto it : comp_size)
+        {
+            ans += it * (n - included_nodes - it);
+            included_nodes += it;
         }
         return ans;
     }

553e98eee875d2f98fb3fdaabb2a5b8ca3442dc2
Akash Biswas <akash41bt@gmail.com>
2025-07-25 00:50:16 +0600

    solve LeetCode 1319: using BFS

--- 1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected.cpp
@@ -1,36 +1,55 @@
-class Solution {
+class Solution
+{
 public:
     int extraEdge = 0, dis_component = 0;
 
-    void dfs(unordered_map<int, vector<int>> &adj, vector<bool> &vis, int u, int parent){
+    void bfs(unordered_map<int, vector<int>> &adj, vector<bool> &vis, int u)
+    {
         vis[u] = true;
+        queue<pair<int, int>> q;
+        q.push({u, -1});
 
-        for(auto neigh: adj[u]){
-            if(vis[neigh] && neigh != parent){
-                extraEdge++;
+        while (!q.empty())
+        {
+            u = q.front().first;
+            int parent = q.front().second;
+
+            for (auto neigh : adj[u])
+            {
+                if (vis[neigh] && neigh != parent)
+                {
+                    extraEdge++;
+                }
+                if (!vis[neigh])
+                {
+                    vis[neigh] = true;
+                    q.push({neigh, u});
+                }
             }
-            if(!vis[neigh]){
-                dfs(adj, vis, neigh, u);
-            }
+            q.pop();
         }
     }
 
-    int makeConnected(int n, vector<vector<int>>& connections) {
+    int makeConnected(int n, vector<vector<int>> &connections)
+    {
         unordered_map<int, vector<int>> adj;
 
-        for(auto edge: connections){
+        for (auto edge : connections)
+        {
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
         vector<bool> vis(n, false);
 
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                dfs(adj, vis, i, -1);
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                bfs(adj, vis, i);
                 dis_component++;
             }
         }
-        extraEdge/=2;
-        return (extraEdge >= dis_component - 1) ? dis_component-1: -1;
+        extraEdge /= 2;
+        return (extraEdge >= dis_component - 1) ? dis_component - 1 : -1;
     }
 };
